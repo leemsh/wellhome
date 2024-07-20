8381da787c73544ce28bda4ed54a79f06af01ccd
Luke <g27776804@gmail.com>
2024-07-20 22:53:38 +0900

    change something

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
 from . import views
-from . import video_view
 
 router = routers.DefaultRouter()
 router.register('Item', views.ItemViewSet)
